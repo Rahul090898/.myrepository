e782789057959bda206d26ab68c39e052e2552ab
Rahul090898 <90690103+Rahul090898@users.noreply.github.com>
2021-09-14 17:33:01 +0530

    Add files via upload
    This is my first Java Program

---
+++ FirstApplication.java
@@ -0,0 +1,7 @@
+class FirstApplication 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("This is my first Java Application."+" "+"I am learning Java."+" "+"A Message: Hello from Java");
+	}
+}
