bf591d89d943587cf9d04fa857e047d8869695ff
Rahul090898 <90690103+Rahul090898@users.noreply.github.com>
2021-10-01 13:07:28 +0530

    Delete FirstApplication.java

--- FirstApplication.java
+++
@@ -1,7 +0,0 @@
-class FirstApplication 
-{
-	public static void main(String[] args) 
-	{
-		System.out.println("This is my first Java Application."+" "+"I am learning Java."+" "+"A Message: Hello from Java");
-	}
-}
